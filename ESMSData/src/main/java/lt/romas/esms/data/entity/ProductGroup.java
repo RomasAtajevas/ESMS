438c61d8026a049e28b3cdc6d50efbadd7594b05
RomasAtajevas <r.atajevas@gmail.com>
2017-01-18 23:30:52 +0200

    Added ProductGroup entity

---
+++ ESMSData/src/main/java/lt/romas/esms/data/entity/ProductGroup.java
@@ -0,0 +1,32 @@
+package lt.romas.esms.data.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(indexes = { @Index(columnList = "parentId") })
+public class ProductGroup implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private Integer parentId;
+
+	@Column(unique = true, nullable = false)
+	private String name;
+
+	@Column(unique = true, nullable = false)
+	private String path;
+}
