4ebb9ed2b85f14ea8abdbb47107290b281a39502
RomasAtajevas <r.atajevas@gmail.com>
2017-01-18 23:51:28 +0200

    Added Characteristic entity

---
+++ ESMSData/src/main/java/lt/romas/esms/data/entity/Characteristic.java
@@ -0,0 +1,24 @@
+package lt.romas.esms.data.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import lombok.Data;
+
+@Data
+@Entity
+public class Characteristic implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(unique = true, nullable = false)
+	private String name;
+}
