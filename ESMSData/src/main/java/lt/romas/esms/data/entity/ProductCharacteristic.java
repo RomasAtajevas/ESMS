ddee1d6dbdae8ad079b77de6ab2314fbedd9a9bf
RomasAtajevas <r.atajevas@gmail.com>
2017-01-18 23:52:23 +0200

    Added ProductCharacteristic entity

---
+++ ESMSData/src/main/java/lt/romas/esms/data/entity/ProductCharacteristic.java
@@ -0,0 +1,36 @@
+package lt.romas.esms.data.entity;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(uniqueConstraints = { @UniqueConstraint(columnNames = { "productId", "characteristicId" }) })
+public class ProductCharacteristic implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	@Column(nullable = false)
+	private Integer productId;
+
+	@ManyToOne
+	@JoinColumn(name = "characteristicId", referencedColumnName = "id", nullable = false)
+	private Characteristic characteristic;
+
+	@Column(nullable = false)
+	private String value;
+}
