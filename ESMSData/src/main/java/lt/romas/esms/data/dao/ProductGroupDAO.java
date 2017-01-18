403b2bea6129d4a378c7e0e9984e58fd5e3b405e
RomasAtajevas <r.atajevas@gmail.com>
2017-01-19 00:01:12 +0200

    Added ProductGroupDAO repository
    For now just an interface with few methods:
    - findTop()
    - findByParentId()

---
+++ ESMSData/src/main/java/lt/romas/esms/data/dao/ProductGroupDAO.java
@@ -0,0 +1,18 @@
+package lt.romas.esms.data.dao;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import lt.romas.esms.data.entity.ProductGroup;
+
+public interface ProductGroupDAO extends JpaRepository<ProductGroup, Integer> {
+
+	@Query("SELECT pg FROM ProductGroup pg WHERE pg.parentId IS NULL ORDER BY pg.name ASC")
+	List<ProductGroup> findTop();
+
+	@Query("SELECT pg FROM ProductGroup pg WHERE pg.parentId = :parentId ORDER BY pg.name ASC")
+	List<ProductGroup> findByParentId(@Param("parentId") Integer parentId);
+}
