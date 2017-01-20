468a1ee7a861cc3e097fff59ede89346d98eb59a
RomasAtajevas <r.atajevas@gmail.com>
2017-01-20 21:51:09 +0200

    Edited ProductGroupService
    Moved creation of list after parent check.

--- ESMSService/src/main/java/lt/romas/esms/service/impl/ProductGroupServiceImpl.java
+++ ESMSService/src/main/java/lt/romas/esms/service/impl/ProductGroupServiceImpl.java
@@ -32,11 +32,11 @@ public class ProductGroupServiceImpl implements ProductGroupService {
 
 	@Override
 	public void createSubGroups(int parentId, int start, int max) {
-		List<ProductGroup> groups = new ArrayList<>();
 		ProductGroup parent = productGroupDAO.findOne(parentId);
 		if (parent == null) {
 			throw new RuntimeException("Parent Group [ID=" + parentId + "] does no exist.");
 		}
+		List<ProductGroup> groups = new ArrayList<>();
 		IntStream.range(start, start + 10).forEach(i -> {
 			ProductGroup group = new ProductGroup();
 			group.setName("Sub Group " + i);
