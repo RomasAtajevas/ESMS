2d125dbbe8e27b052e868cb49b7765e342c9f166
RomasAtajevas <r.atajevas@gmail.com>
2017-01-20 18:08:37 +0200

    Added ProductController

---
+++ ESMSWeb/src/main/java/lt/romas/esms/web/controller/ProductController.java
@@ -0,0 +1,25 @@
+package lt.romas.esms.web.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import lt.romas.esms.data.entity.Product;
+import lt.romas.esms.service.ProductService;
+
+@RestController
+@RequestMapping(value = "/product")
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+
+	@RequestMapping(value = "/findByGroupId/{groupId}", method = RequestMethod.GET)
+	private List<Product> findByGroupId(@PathVariable("groupId") Integer groupId) {
+		return productService.findByGroupId(groupId);
+	}
+}
