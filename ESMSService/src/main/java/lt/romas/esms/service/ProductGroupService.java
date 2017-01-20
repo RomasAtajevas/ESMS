cfccb9b12a9d9ec17e00f3a6782af28d42eb6eb3
RomasAtajevas <r.atajevas@gmail.com>
2017-01-20 21:45:47 +0200

    Edited ProductGroupService
    Added few methods responsible for creation of top and sub groups. Used
    as test data.

--- ESMSService/src/main/java/lt/romas/esms/service/ProductGroupService.java
+++ ESMSService/src/main/java/lt/romas/esms/service/ProductGroupService.java
@@ -6,6 +6,10 @@ import lt.romas.esms.data.entity.ProductGroup;
 
 public interface ProductGroupService {
 
+	void createTopGroups();
+
+	void createSubGroups(int parentId, int start, int max);
+
 	List<ProductGroup> findTop();
 
 	List<ProductGroup> findByParentId(int parentId);
--- ESMSService/src/main/java/lt/romas/esms/service/impl/ProductGroupServiceImpl.java
+++ ESMSService/src/main/java/lt/romas/esms/service/impl/ProductGroupServiceImpl.java
@@ -1,6 +1,8 @@
 package lt.romas.esms.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,6 +18,34 @@ public class ProductGroupServiceImpl implements ProductGroupService {
 
 	@Autowired
 	private ProductGroupDAO productGroupDAO;
+
+	@Override
+	public void createTopGroups() {
+		List<ProductGroup> groups = new ArrayList<>();
+		IntStream.range(1, 11).forEach(i -> {
+			ProductGroup group = new ProductGroup();
+			group.setName("Group " + i);
+			groups.add(group);
+		});
+		productGroupDAO.save(groups);
+	}
+
+	@Override
+	public void createSubGroups(int parentId, int start, int max) {
+		List<ProductGroup> groups = new ArrayList<>();
+		ProductGroup parent = productGroupDAO.findOne(parentId);
+		if (parent == null) {
+			throw new RuntimeException("Parent Group [ID=" + parentId + "] does no exist.");
+		}
+		IntStream.range(start, start + 10).forEach(i -> {
+			ProductGroup group = new ProductGroup();
+			group.setName("Sub Group " + i);
+			group.setParentId(parentId);
+			group.setPath((parent.getPath() == null ? "" : parent.getPath() + ".") + parentId);
+			groups.add(group);
+		});
+		productGroupDAO.save(groups);
+	}
 
 	@Override
 	public List<ProductGroup> findTop() {
