2229297fa8ab8bb1ccf0a95c48365c1c7a49790b
RomasAtajevas <r.atajevas@gmail.com>
2017-01-20 18:07:54 +0200

    Added ProductService

---
+++ ESMSService/src/main/java/lt/romas/esms/service/ProductService.java
@@ -0,0 +1,10 @@
+package lt.romas.esms.service;
+
+import java.util.List;
+
+import lt.romas.esms.data.entity.Product;
+
+public interface ProductService {
+
+	List<Product> findByGroupId(Integer groupId);
+}
---
+++ ESMSService/src/main/java/lt/romas/esms/service/impl/ProductServiceImpl.java
@@ -0,0 +1,24 @@
+package lt.romas.esms.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lt.romas.esms.data.dao.ProductDAO;
+import lt.romas.esms.data.entity.Product;
+import lt.romas.esms.service.ProductService;
+
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService {
+
+	@Autowired
+	private ProductDAO productDAO;
+
+	@Override
+	public List<Product> findByGroupId(Integer groupId) {
+		return productDAO.findByGroupId(groupId);
+	}
+}
