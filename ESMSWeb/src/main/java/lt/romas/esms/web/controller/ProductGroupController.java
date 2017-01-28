67ca215ddce754ad72cf59bb437f1bb65d989caa
RomasAtajevas <r.atajevas@gmail.com>
2017-01-28 22:27:39 +0200

    Added "CrossOrigin" annotation to controllers
    This annotation is needed to allow access from different host as I'm
    developing front end stuff on node.js

--- ESMSWeb/src/main/java/lt/romas/esms/web/controller/ProductGroupController.java
+++ ESMSWeb/src/main/java/lt/romas/esms/web/controller/ProductGroupController.java
@@ -3,6 +3,7 @@ package lt.romas.esms.web.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import lt.romas.esms.data.entity.ProductGroup;
 import lt.romas.esms.service.ProductGroupService;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping(value = "/productGroup")
 public class ProductGroupController {
